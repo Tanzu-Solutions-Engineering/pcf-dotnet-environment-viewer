b441659419ccd013267ae0ef67bec02f2765cd38
Aaron Fortener <afortener@pivotal.io>
2015-09-02 10:39:54 -0400

    added bat file

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
-APP_NAME=env
+export APP_NAME=environment
 cf delete $APP_NAME -r -f
 cf push $APP_NAME -m 2g -s windows2012R2 -b https://github.com/ryandotsmith/null-buildpack.git --no-start -p ./ViewEnvironment/
 cf enable-diego $APP_NAME
